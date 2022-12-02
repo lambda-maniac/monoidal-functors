4b49cc65e0122bcb727f6198f1bd3b7f1bf9d619
Sr. Meia-noiteλ <lordzarkares@Outlook.com>
2022-12-02 17:54:43 -0300

    |

---
+++ Monoidal.hs
@@ -0,0 +1,17 @@
+module Monoidal where
+
+class Functor f => Monoidal f where
+    unit  :: f ()                   -- Identity
+    (<^>) :: f a -> f b -> f (a, b) -- Tensor product
+
+-- Applicative's (<*>)
+(<@>) :: Monoidal f => f (a -> b) -> f a -> f b
+(<@>) ff fa = uncurry ($) <$> ff <^> fa
+
+-- Monoidal instance for the arrow functor, allowing (<@>) to behave like `f a b c = a c (b c)`.
+instance Monoidal ((->) r) where
+    unit :: r -> ()
+    unit r = ()
+
+    (<^>) :: (r -> a) -> (r -> b) -> r -> (a, b)
+    (<^>) ra rb r = ((ra r), (rb r))
